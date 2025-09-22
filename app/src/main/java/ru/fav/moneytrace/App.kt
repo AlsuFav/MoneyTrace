2e93205b3a8e310dccf6b30fa88bf1f8d031dc93
Alsu Favarisova <143123118+AlsuFav@users.noreply.github.com>
2025-09-22 17:38:28 +0300

    some fixes

--- app/src/main/java/ru/fav/moneytrace/App.kt
+++ app/src/main/java/ru/fav/moneytrace/App.kt
@@ -1,31 +1,11 @@
 package ru.fav.moneytrace
 
 import android.app.Application
-import androidx.work.Configuration
-import androidx.work.WorkManager
 import dagger.hilt.android.HiltAndroidApp
-import ru.fav.moneytrace.workmanager.HiltWorkerFactory
-import ru.fav.moneytrace.workmanager.SyncManager
-import javax.inject.Inject
 
 @HiltAndroidApp
 class App : Application() {
-
-    @Inject
-    lateinit var workerFactory: HiltWorkerFactory
-
-    @Inject
-    lateinit var syncManager: SyncManager
-
     override fun onCreate() {
         super.onCreate()
-
-        val config = Configuration.Builder()
-            .setWorkerFactory(workerFactory)
-            .build()
-
-        WorkManager.initialize(this, config)
-
-        syncManager.schedulePeriodicSync()
     }
 }
--- app/src/main/java/ru/fav/moneytrace/workmanager/HiltWorkerFactory.kt
+++
@@ -1,9 +0,0 @@
-package ru.fav.moneytrace.workmanager
-
-import androidx.work.DelegatingWorkerFactory
-import javax.inject.Inject
-import javax.inject.Singleton
-
-@Singleton
-class HiltWorkerFactory @Inject constructor() : DelegatingWorkerFactory() {
-}
